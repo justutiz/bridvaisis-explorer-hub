1d27e256c1641c8367eb38a2748d37c7d2cd1cbb
Justas Maziliauskas <justas.maziliauskas@digitalaz.com>
2025-05-14 17:00:04 +0300

    Revert "Fix: Blank screen after CSS refactor"
    This reverts commit 1aa98e4c5d68cf1280a6c502f7f853b73a677332.

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// Import CSS files
+// Import CSS files - order matters for cascade
 import './index.css';
 
 const queryClient = new QueryClient();
